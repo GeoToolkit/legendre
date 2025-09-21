063f7db31c9f6725a1961d5cd3c69e9526cb7096
Yura <>
2025-09-21 17:02:32 +0300

    adds diagonal recursion

--- src/main/java/com/geotoolkit/recurrent/PolynomialsCalculator.java
+++ src/main/java/com/geotoolkit/recurrent/PolynomialsCalculator.java
@@ -4,29 +4,37 @@ public class PolynomialsCalculator {
 
   /**
    * Initiates array of appropriate length with two first elements.
-   * @param degreeOrder maximum degree/order of desired associated Legendre functions
-   * @param u cos of latitude.
+   *
+   * @param degreeOrder maximum degree/order of desired fully normalized associated Legendre functions
+   * @param u           cos of latitude.
    * @return initial linear array with two first elements.
    */
   double[] initiatePnm(int degreeOrder, double u) {
     degreeOrder++;
     double p00 = 1.0d;
-    double p10 = Math.sqrt(3) * u;
+    double p11 = Math.sqrt(3) * u;
     double[] pnm = new double[(degreeOrder * degreeOrder + degreeOrder) / 2];
-    pnm[f(0,0)] = p00;
-    pnm[f(1,0)] = p10;
+    pnm[f(0, 0)] = p00;
+    pnm[f(1, 1)] = p11;
     return pnm;
   }
 
-  void diagonalRecursion(double Pm_1m_1, int m, double u) {
-
+  double diagonalRecursion(double pm_1m_1, int m, double u) {
+    return u * Math.sqrt((double)(2 * m + 1)/(2 * m)) * pm_1m_1;
   }
+
+  double columnRecursion(double pn_1m, double pn_2m, int n, int m, double t) {
+    double anm = Math.sqrt(((double)(2 * n - 1)*(2 * n +1)) / ((n -m) * (n + m)));
+    double bnm = Math.sqrt(((double)(2 * n + 1) * (n + m - 1) * (n - m -1)) / ((n - m) * (n + m) * (2 * n - 3)));
+  return anm * t * pn_1m - bnm * pn_2m;
+  }
+
   /**
    * Transforms 2D triangular array into 1D linear array. Starts from zero index.
    *
    * @return index of analogous linear array element.
    */
   int f(int n, int m) {
-    return (n * n +  n) / 2 + m;
+    return (n * n + n) / 2 + m;
   }
 }
--- src/test/java/com/geotoolkit/recurrent/PolynomialsCalculatorTest.java
+++ src/test/java/com/geotoolkit/recurrent/PolynomialsCalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class PolynomialsCalculatorTest {
 
+  double delta = 0.000_000_000_000_000_1;
   @Test
   void f_whenNEquals3AndMEquals2_thenReturn12() {
     int n = 3;
@@ -22,7 +23,38 @@ public class PolynomialsCalculatorTest {
     PolynomialsCalculator pCalc = new PolynomialsCalculator();
     double[] pnm = pCalc.initiatePnm(order, u);
     Assertions.assertEquals(15, pnm.length);
-    Assertions.assertEquals(1.0, pnm[0],0.000_000_000_000_000_1);
-    Assertions.assertEquals(Math.sqrt(3) * u, pnm[1],0.000_000_000_000_000_1);
+    Assertions.assertEquals(1.0, pnm[0], delta);
+    Assertions.assertEquals(Math.sqrt(3) * u, pnm[2], delta);
+  }
+
+  @Test
+  void diagonalRecursion_testAFL22() {
+    double phi = 0.5474567;
+    double u = Math.cos(phi);
+    double p22 = 3 * u * u;
+    double expected_fnp22 = p22 * fnfactor(2, 2);
+    PolynomialsCalculator pCalc = new PolynomialsCalculator();
+
+    double[] fnalf = pCalc.initiatePnm(2, u);
+    double fnp22 = pCalc.diagonalRecursion(fnalf[2],2, u);
+
+    Assertions.assertEquals(expected_fnp22, fnp22, delta*100);
+
+  }
+
+  private long factorial(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    if (n <= 2) {
+      return n;
+    }
+    return n * factorial(n - 1);
+  }
+
+  private double fnfactor(int n, int m) {
+    double factorialFraction = (double) factorial(n - m) / factorial(n + m);
+    int k = m == 0 ? 1 : 2;
+    return Math.sqrt(k * (2 * n + 1) * factorialFraction);
   }
 }
